35694f8a0976023efba13e8deb503df0e9c4095b
max-shymanskiy <maxshymanskiy@gmail.com>
2025-03-26 10:18:25 +0200

    final changes

--- LR_03.2rec/LR_03.2rec.cpp
+++ LR_03.2rec/LR_03.2rec.cpp
@@ -12,53 +12,53 @@ typedef int Info;
 void enqueue(Elem*& first, Elem*& last, Info value)
 {
     Elem* tmp = new Elem;  //1
-	tmp->info = value;    //2
-	tmp->link = NULL;   //3
-	if (last != NULL)   //4
-		last->link = tmp;  //5
-	last = tmp;		//6
-	if (first == NULL)  //7
-		first = tmp;   //8
+    tmp->info = value;    //2
+    tmp->link = NULL;   //3
+    if (last != NULL)   //4
+        last->link = tmp;  //5
+    last = tmp;     //6
+    if (first == NULL)  //7
+        first = tmp;   //8
 }
 
 Info dequeue(Elem*& first, Elem*& last)
 {
-	Elem* tmp = first->link; //1
-	Info value = first->info; //2
-	delete first; //3
-	first = tmp; //4
-	if (first == NULL) //5
-		last = NULL; //6
-	return value;   //7
+    Elem* tmp = first->link; //1
+    Info value = first->info; //2
+    delete first; //3
+    first = tmp; //4
+    if (first == NULL) //5
+        last = NULL; //6
+    return value;   //7
 }
 
 void reverseList(Elem*& first, Elem*& last, Elem* current = NULL, Elem* prev = NULL)
 {
-    if (current == NULL) {  // Початковий виклик
+    if (current == NULL) {
         if (first == NULL) return;
-        last = first;      // Новий кінець - старий початок
-        reverseList(first, last, first, NULL);
-        return;
+        current = first;
+        last = first;
     }
-	Elem* next = current->link; // Запам'ятовуємо наступний елемент
+
+    Elem* next = current->link;
     current->link = prev;
 
-    if (next == NULL) {    // Досягли кінця оригінального списку
-        first = current;   // Оновлюємо початок
+    if (next == NULL) {
+        first = current;
+        return;
     }
-    else {
-        reverseList(first, last, next, current); // Рекурсивний виклик
-    }
+
+    reverseList(first, last, next, current);
 }
 
 void printList(Elem* first)
 {
-    Elem* current = first;
-    while (current != NULL) {
-        cout << current->info << " ";
-        current = current->link;
+    if (first == NULL) {
+        cout << endl;
+        return;
     }
-    cout << endl;
+    cout << first->info << " ";
+    printList(first->link);
 }
 
 int main()
